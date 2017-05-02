8ef1122838b3abcb5eebd5fe31150d1e428db461
Rickard <dunklas@gmail.com>
2017-05-02 16:23:35 +0200

    Add widgets to next trip-layout.

--- app/src/main/java/com/example/reseplaneraren2/controllers/searchtrip/SearchTripController.java
+++ app/src/main/java/com/example/reseplaneraren2/controllers/searchtrip/SearchTripController.java
@@ -5,18 +5,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
 
 import com.example.reseplaneraren2.R;
 
-/**
- * Created by christoffer on 2017-05-02.
- */
-
 public class SearchTripController extends Fragment {
+
+    private AutoCompleteTextView searchStopField;
+    private ListView nearbyStopsList;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.search_trip_layout, container, false);
+        View view = inflater.inflate(R.layout.search_trip_layout, container, false);
 
+        searchStopField = (AutoCompleteTextView) view.findViewById(R.id.searchStopField);
+        nearbyStopsList = (ListView) view.findViewById(R.id.nearbyStopsList);
+
+        return view;
     }
 
 }
