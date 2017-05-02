fc043c4be70773d7767d92614866eb79a6174d1a
Rickard <dunklas@gmail.com>
2017-05-02 15:25:19 +0200

    Add fragment attribute to Screen-enum. Change import statement in controllers (to support.v4.app.*)

--- app/src/main/java/com/example/reseplaneraren2/Screen.java
+++ app/src/main/java/com/example/reseplaneraren2/Screen.java
@@ -1,9 +1,26 @@
 package com.example.reseplaneraren2;
 
+import android.support.v4.app.Fragment;
+
+import com.example.reseplaneraren2.controllers.favorites.FavoritesController;
+import com.example.reseplaneraren2.controllers.nexttrip.NextTripController;
+import com.example.reseplaneraren2.controllers.searchtrip.SearchTripController;
+import com.example.reseplaneraren2.controllers.ticket.TicketController;
+
 public enum Screen {
 
-    SEARCH_TRIP,
-    NEXT_TRIP,
-    FAVORITES,
-    TICKET,
+    SEARCH_TRIP(new SearchTripController()),
+    NEXT_TRIP(new NextTripController()),
+    FAVORITES(new FavoritesController()),
+    TICKET(new TicketController());
+
+    private Fragment fragment;
+
+    Screen(Fragment fragment) {
+        this.fragment = fragment;
+    }
+
+    public Fragment getFragment() {
+        return fragment;
+    }
 }
--- app/src/main/java/com/example/reseplaneraren2/controllers/ticket/TicketController.java
+++ app/src/main/java/com/example/reseplaneraren2/controllers/ticket/TicketController.java
@@ -4,7 +4,7 @@ package com.example.reseplaneraren2.controllers.ticket;
  * Created by christoffer on 2017-05-02.
  */
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
