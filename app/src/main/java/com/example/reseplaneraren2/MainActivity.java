a3b867022dd2ff4b6551925c89193d71232d5739
Rickard <dunklas@gmail.com>
2017-05-02 14:40:54 +0200

    Add bottom navigation

--- app/src/main/java/com/example/reseplaneraren2/MainActivity.java
+++ app/src/main/java/com/example/reseplaneraren2/MainActivity.java
@@ -1,9 +1,38 @@
 package com.example.reseplaneraren2;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView navigationView;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_search_trip:
+                    // Do something
+                    return true;
+                case R.id.navigation_next_trip:
+                    // Do something
+                    return true;
+                case R.id.navigation_favorites:
+                    // Do something
+                    return true;
+                case R.id.navigation_ticket:
+                    // Do something
+                    return true;
+            }
+            return false;
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
