9e00611add40eeec7c535c336d9a7c60ab270b4b
christoffermagnusson <christoffer88@live.se>
2017-05-03 11:15:37 +0200

    Added interfaces for data handling

---
+++ app/src/main/java/com/example/reseplaneraren2/data/IDepartureHandler.java
@@ -0,0 +1,16 @@
+package com.example.reseplaneraren2.data;
+
+import com.example.reseplaneraren2.model.Departure;
+
+import java.util.ArrayList;
+
+/**
+ * Created by christoffer on 2017-05-03.
+ */
+
+public interface IDepartureHandler {
+
+        public void receiveDeparture(ArrayList<Departure> departureList);
+
+        public void receiveDepartureError(String message);
+}
---
+++ app/src/main/java/com/example/reseplaneraren2/data/IStopLocationHandler.java
@@ -0,0 +1,18 @@
+package com.example.reseplaneraren2.data;
+
+import com.example.reseplaneraren2.model.StopLocation;
+
+import java.util.ArrayList;
+
+/**
+ * Created by christoffer on 2017-05-03.
+ */
+
+public interface IStopLocationHandler {
+
+    public void receiveStopLocation(ArrayList<StopLocation> stopLocList);
+
+    public void receiveStopLocationError(String message);
+
+
+}
