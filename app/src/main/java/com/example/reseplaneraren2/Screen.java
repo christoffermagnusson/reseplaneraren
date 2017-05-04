59132f8747ece77aec0efbbb838e0e1b2bfae749
alajv <williamlidholm1@gmail.com>
2017-05-04 13:02:27 +0200

    Ticket har fått knappar som öppnar fragments

--- app/src/main/java/com/example/reseplaneraren2/Screen.java
+++ app/src/main/java/com/example/reseplaneraren2/Screen.java
@@ -6,6 +6,7 @@ import com.example.reseplaneraren2.controllers.departuredisplay.DepartureDisplay
 import com.example.reseplaneraren2.controllers.favorites.FavoritesController;
 import com.example.reseplaneraren2.controllers.nexttrip.NextTripController;
 import com.example.reseplaneraren2.controllers.searchtrip.SearchTripController;
+import com.example.reseplaneraren2.controllers.ticket.MyTicketFragment;
 import com.example.reseplaneraren2.controllers.ticket.TicketController;
 
 public enum Screen {
@@ -15,7 +16,8 @@ public enum Screen {
     FAVORITES(new FavoritesController()),
     TICKET(new TicketController()),
 
-    DEPARTURE_DISPLAY (new DepartureDisplayController());
+    DEPARTURE_DISPLAY (new DepartureDisplayController()),
+    MY_TICKET_DISPLAY (new MyTicketFragment());
 
     private Fragment fragment;
 
