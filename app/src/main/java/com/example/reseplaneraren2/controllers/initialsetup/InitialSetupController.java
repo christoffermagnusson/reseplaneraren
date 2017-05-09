7ba908530348a0b20dd465294e02dabccc419611
christoffermagnusson <christoffer88@live.se>
2017-05-09 14:18:37 +0200

    Outcommented some stuff that causes errors (xml) drawable_logotype

--- app/src/main/java/com/example/reseplaneraren2/controllers/initialsetup/InitialSetupController.java
+++ app/src/main/java/com/example/reseplaneraren2/controllers/initialsetup/InitialSetupController.java
@@ -31,7 +31,7 @@ public class InitialSetupController extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.layout.initial_setup,container,false);
+        View view = inflater.inflate(R.layout.initial_setup,container,false);
 
         initialSetupAdapter = new InitialSetupAdapter(getActivity().getSupportFragmentManager());
         initialSetupViewPager = (ViewPager)view.findViewById(R.id.initialVP);
