43c2de27e33fefa9e9bacceb1bbe7983e44d49cd
christoffermagnusson <christoffer88@live.se>
2017-05-10 12:45:19 +0200

    Fixed the horizontal swiping menu on the initial startup screen!

--- app/src/main/java/com/example/reseplaneraren2/Screen.java
+++ app/src/main/java/com/example/reseplaneraren2/Screen.java
@@ -4,6 +4,7 @@ import android.support.v4.app.Fragment;
 
 import com.example.reseplaneraren2.controllers.departuredisplay.DepartureBoardDisplayController;
 import com.example.reseplaneraren2.controllers.favorites.FavoritesController;
+import com.example.reseplaneraren2.controllers.initialsetup.InitialSetupController;
 import com.example.reseplaneraren2.controllers.nexttrip.NextTripController;
 import com.example.reseplaneraren2.controllers.searchtrip.SearchTripController;
 import com.example.reseplaneraren2.controllers.ticket.MyTicketFragment;
@@ -11,7 +12,7 @@ import com.example.reseplaneraren2.controllers.ticket.TicketController;
 
 public enum Screen {
 
-    //INITIAL_SETUP(new InitialSetupController()),
+    INITIAL_SETUP(new InitialSetupController()),
 
     SEARCH_TRIP(new SearchTripController()),
     NEXT_TRIP(new NextTripController()),
