65093065c910a1223025e545e63e8d738b73a296
christoffermagnusson <christoffer88@live.se>
2017-05-17 12:21:21 +0200

    More attributes added to Screens. identifier for naming

--- app/src/main/java/com/example/reseplaneraren2/MainActivity.java
+++ app/src/main/java/com/example/reseplaneraren2/MainActivity.java
@@ -188,6 +188,9 @@ public class MainActivity extends AppCompatActivity {
         inflate(screenToDisplay);
     }
 
+
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
